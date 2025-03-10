a9b201ac7f90f4fcee12647f62151630c0fb8d57
ashwin925 <ashwinsundar90250@gmail.com>
2025-03-10 16:25:17 +0530

    upyt

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import "./Register.css";
 
 const Register = () => {
   const [formData, setFormData] = useState({
