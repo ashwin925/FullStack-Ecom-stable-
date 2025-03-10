a2a35ebbaf692013de7fad1a74bf419ee35967c3
ashwin925 <ashwinsundar90250@gmail.com>
2025-03-10 15:47:45 +0530

    upt

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,3 +1,36 @@
+import asyncHandler from 'express-async-handler';
+import User from '../models/User.js';
+import jwt from 'jsonwebtoken';
+
+// Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
+
+// @desc    Register user
+// @route   POST /api/auth/register
+export const register = asyncHandler(async (req, res) => {
+  const { name, email, password, role } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const user = await User.create({ name, email, password, role });
+  
+  res.status(201).json({
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+    token: generateToken(user._id),
+  });
+});
+
 // Login controller example
 const login = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
