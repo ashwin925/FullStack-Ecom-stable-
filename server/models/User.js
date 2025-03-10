c9b833063e8331ce59bea9b2681eeb8dfef60cad
ashwin925 <ashwinsundar90250@gmail.com>
2025-03-10 07:21:25 +0530

    user fix

--- server/models/User.js
+++ server/models/User.js
@@ -6,11 +6,10 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, enum: ['user', 'admin'], default: 'user' },
-  createdAt: { type: Date, default: Date.now }
 });
 
-// The Hash password before saving
-userSchema.pre('save', async function(next) {
+// Hash password before saving
+userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
