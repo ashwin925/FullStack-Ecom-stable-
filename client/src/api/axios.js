4fd50dafa415eb59973250dd11a5a64c0b6c58f0
ashwin925 <ashwinsundar90250@gmail.com>
2025-03-10 15:38:22 +0530

    upt

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,14 +1,17 @@
 import axios from 'axios';
 
-const instance = axios.create({
-  baseURL: 'http://localhost:5000/api', // Backend URL
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  withCredentials: true
 });
 
-// Add JWT token to headers
-instance.interceptors.request.use((config) => {
+// Add request interceptor to include JWT token
+axiosInstance.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
   return config;
 });
 
-export default instance;
+export default axiosInstance;
