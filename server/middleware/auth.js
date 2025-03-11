6717dbbb320b4ef818eac7ac90095945dba908f2
ashwin925 <ashwinsundar90250@gmail.com>
2025-03-11 07:40:01 +0530

    upt

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,9 +4,11 @@ import User from '../models/User.js';
 
 const protect = asyncHandler(async (req, res, next) => {
   let token;
-  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+  
+  // Get token from cookie
+  if (req.cookies.token) {
     try {
-      token = req.headers.authorization.split(' ')[1];
+      token = req.cookies.token;
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = await User.findById(decoded.id).select('-password');
       next();
@@ -16,6 +18,7 @@ const protect = asyncHandler(async (req, res, next) => {
       throw new Error('Not authorized, token failed');
     }
   }
+
   if (!token) {
     res.status(401);
     throw new Error('Not authorized, no token');
