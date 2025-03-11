32cf4053ae40dde442fdcf281a4af9006338abcf
ashwin925 <ashwinsundar90250@gmail.com>
2025-03-11 19:48:32 +0530

    upt

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000',
-  withCredentials: true,
+  withCredentials: true, 
 });
-
 
 export default axiosInstance;
