d3b691f04ae09057f58df6f2d1ea49312125a9f6
ashwin925 <ashwinsundar90250@gmail.com>
2025-03-11 05:56:47 +0530

    upt

--- server/models/User.js
+++ server/models/User.js
@@ -1,22 +1,24 @@
-import mongoose from 'mongoose';
-import bcrypt from 'bcryptjs';
-
 const userSchema = new mongoose.Schema({
-  // ... existing schema
-});
-
-// Add password matching method
-userSchema.methods.matchPassword = async function(enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
-
-// Add pre-save hook for password hashing
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) {
-    next();
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ['user', 'seller', 'admin'],
+    default: 'user'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
   }
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
 });
-
-export default mongoose.model('User', userSchema);
