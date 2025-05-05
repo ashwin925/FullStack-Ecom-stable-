150675ae4df380b2e3c1230d2ee7427f5c595fdf
ashwin925 <ashwinsundar90250@gmail.com>
2025-05-05 15:32:54 +0530

    full set ready

--- client/src/services/emailService.js
+++ client/src/services/emailService.js
@@ -1,15 +1,14 @@
 import emailjs from '@emailjs/browser';
 
-// Initialize with your public key
 emailjs.init(import.meta.env.VITE_EMAILJS_PUBLIC_KEY);
 
 export const sendUserEmail = async (userEmail, userName, actionType) => {
   try {
     const templateParams = {
-      user_email: userEmail, // Will go to the logged-in user
+      user_email: userEmail, // Only goes to the user
       name: userName,
       action: actionType,
-      reply_to: 'ashwinsundar90250@gmail.com' // Replies come here
+      reply_to: 'ashwinsundar90250@gmail.com' // Only affects replies
     };
 
     await emailjs.send(
@@ -19,6 +18,5 @@ export const sendUserEmail = async (userEmail, userName, actionType) => {
     );
   } catch (error) {
     console.error('Email sending failed:', error);
-    // Silent fail - don't interrupt user experience
   }
 };
